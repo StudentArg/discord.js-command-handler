538bdb42ca38289b4b800565311e21f1f253319e
not-root <77559597+not-root@users.noreply.github.com>
2021-05-23 22:59:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,32 @@ require(`./handler/${handler}`)(client)
 
 client.on('ready', () => console.log('Bot encendido!'))
 
+client.on('message', async message => {
+if (message.author.bot) return
+ if(!message.guild) return;
+
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  
+  const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
+
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  
+  const command = args.shift().toLowerCase();
+
+  const cmd = client.commands.get(command);
+  
+  const aliases = client.commands.find(x => x.info.aliases.includes(command))
+
+
+  if(cmd){
+    cmd.run(client, message, args);
+  } else if(aliases) {
+    aliases.run(client, message, args);
+  } else {
+    return
+  }
+})
+
 client.login(client.config.token)
